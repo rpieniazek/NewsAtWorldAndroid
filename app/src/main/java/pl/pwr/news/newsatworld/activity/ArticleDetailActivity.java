4d7a89abb653cd40ff1a4281e8218492ca27c7fc
rkpieniazek <rkpieniazek@gmail.com>
2016-05-07 18:57:00 +0200

    user can go to article url

--- app/src/main/java/pl/pwr/news/newsatworld/activity/ArticleDetailActivity.java
+++ app/src/main/java/pl/pwr/news/newsatworld/activity/ArticleDetailActivity.java
@@ -1,26 +1,32 @@
-package pl.pwr.news.newsatworld;
+package pl.pwr.news.newsatworld.activity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import pl.pwr.news.newsatworld.R;
 import pl.pwr.news.newsatworld.model.Article;
+import pl.pwr.news.newsatworld.presenter.ArticlePresenter;
+import pl.pwr.news.newsatworld.view.ArticleDetailView;
 
 /**
  * An activity representing a single Article detail screen. This
@@ -28,38 +34,37 @@ import pl.pwr.news.newsatworld.model.Article;
  * item details are presented side-by-side with a list of items
  * in a {@link ArticleListActivity}.
  */
-public class ArticleDetailActivity extends AppCompatActivity {
+public class ArticleDetailActivity extends AppCompatActivity implements ArticleDetailView {
 
     public static final String ARG_ITEM_ID = "ArticleDetailFragment.ARG_ITEM_ID";
 
     private Article article;
+    private ArticlePresenter presenter;
 
+    @Bind(R.id.like)
+    Button likeButton;
+
+    @Bind(R.id.dislike)
+    Button dislikeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_detail);
+        ButterKnife.bind(this);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.detail_toolbar);
         setSupportActionBar(toolbar);
-
-        // Show the Up button in the action bar.
+        allowNetworkOnUIThread();
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
+        presenter = new ArticlePresenter();
         getArticleFromIntent();
-
-
-        // savedInstanceState is non-null when there is fragment state
-        // saved from previous configurations of this activity
-        // (e.g. when rotating the screen from portrait to landscape).
-        // In this case, the fragment will automatically be re-added
-        // to its container so we don't need to manually add it.
-        // For more information, see the Fragments API guide at:
-        //
-        // http://developer.android.com/guide/components/fragments.html
-        //
+        addLikeDislikeOnClickListeners();
+        setupGotoArticleButton();
         if (savedInstanceState == null) {
             // Create the detail fragment and add it to the activity
             // using a fragment transaction.
@@ -70,6 +75,42 @@ public class ArticleDetailActivity extends AppCompatActivity {
             TextView articleText = (TextView) findViewById(R.id.article_detail);
             articleText.setText(article.getText());
         }
+    }
+
+    private void allowNetworkOnUIThread() {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+    }
+
+    private void addLikeDislikeOnClickListeners() {
+        likeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.likeArticle(article.getId());
+                showOpinionSentToast();
+                hideButtons();
+            }
+
+
+        });
+
+        dislikeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.dislikeArticle(article.getId());
+                showOpinionSentToast();
+                hideButtons();
+            }
+        });
+    }
+
+    private void hideButtons() {
+        likeButton.setVisibility(View.GONE);
+        dislikeButton.setVisibility(View.GONE);
+    }
+
+    private void showOpinionSentToast() {
+        Toast.makeText(this, "Opinion sent", Toast.LENGTH_SHORT).show();
     }
 
     private void getArticleFromIntent() {
@@ -117,19 +158,19 @@ public class ArticleDetailActivity extends AppCompatActivity {
     }
 
     private void setupGotoArticleButton() {
-//        LayoutInflater layoutInflater = getLayoutInflater();
-//        View activityLayout = layoutInflater.inflate(R.layout.activity_article_detail,null);
-//        FloatingActionButton fab = (FloatingActionButton)  activityLayout.findViewById(R.id.goto_article);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                String url = article.getLink();
-//                System.out.println(url);
-//                Intent i = new Intent(Intent.ACTION_VIEW);
-//                i.setData(Uri.parse(url));
-//                startActivity(i);
-//            }
-//        });
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.goto_article);
+        if (fab != null) {
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String url = article.getLink();
+                    System.out.println(url);
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                }
+            });
+        }
     }
 
 }
